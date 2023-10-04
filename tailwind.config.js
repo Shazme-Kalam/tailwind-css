0ae518cefd9491d9ad15bf3ae7a6594506191f02
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2023-10-04 11:03:33 -0700

    login page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    }
+  },
+    extend: {
+      colors: {
+        primarycolor: "#F9A826",
+      }
+    },
+    fontFamily: {
+      display: ["Nunito", "sans-serif"],
+    }
+  }
+
+
